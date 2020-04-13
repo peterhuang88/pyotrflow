2a427a9ba590fa22b70b0991d0bf98604239e541
Peter Huang <peterhuang@Peters-MacBook-Pro.local>
2020-04-13 16:07:55 -0700

    Add first draft of layer class

---
+++ layers/Layer.h
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+
+class Layer {
+    public:
+        Layer(int num_neurons);
+        ~Layer();
+        
+    private:
+        double* weights;
+        double* results;
+        double* gradients;
+        int num_neurons;
+        std::string name; 
+};
+
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+
+#include "layers/Layer.h"
+int main(int argc, char** argv) {
+    Layer layer1(5);
+    std::cout << "Test done\n";
+    return 0;
+}
