aa97d280846fe2598d3139f94279f728054af637
Jordan Buckmaster <buckmast@purdue.edu>
2020-04-26 18:26:21 -0500

    Add parallel transpose

--- test/main.cpp
+++ test/main.cpp
@@ -7,10 +7,10 @@
 #include "MatrixCalculator.h"
 
 int main(int argc, char** argv) {
-    int num_rows1 = 50;
-    int num_cols1 = 60;
-    int num_rows2 = 60;
-    int num_cols2 = 50;
+    int num_rows1 = 4;
+    int num_cols1 = 4;
+    int num_rows2 = 4;
+    int num_cols2 = 4;
     double** mat1 = new double*[num_rows1];
     for (int i = 0; i < num_rows1; i++) {
         mat1[i] = new double[num_cols1];
@@ -37,10 +37,17 @@ int main(int argc, char** argv) {
     int numThreads = 16;
     MatrixCalculator mc(numThreads);
 
-    double** temp = mc.matrixTimesMatrix(mat1, num_rows1, num_cols1, mat2, num_rows2, num_cols2);
-    //double** temp = mc.transposeMatrix(mat1, 4, 3);
     for (int i = 0; i < num_rows1; i++) {
-        for (int j = 0; j < num_cols2; j++) {
+        for (int j = 0; j < num_cols1; j++) {
+            printf("%lf ", mat1[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    //double** temp = mc.matrixTimesMatrix(mat1, num_rows1, num_cols1, mat2, num_rows2, num_cols2);
+    double** temp = mc.transposeMatrix(mat1, num_rows1, num_cols1);
+    for (int i = 0; i < num_rows1; i++) {
+        for (int j = 0; j < num_cols1; j++) {
             printf("%lf ", temp[i][j]);
         }
         printf("\n");
