6b358f5b5999ad52d4e8d7ba2a44b9e385243c3d
Jordan Buckmaster <buckmast@purdue.edu>
2020-04-30 14:49:38 -0500

    Fix compile issue in mpi_exp2

--- mpi_parallel_exp2/MatrixCalculator.h
+++ mpi_parallel_exp2/MatrixCalculator.h
@@ -2,11 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <string>
 #include <pthread.h>
 
@@ -14,39 +9,9 @@
 #define MATRIXCALCULATOR_H
 
 //double** mat1, int num_rows1, int num_cols1, double** mat2, int num_rows2, int num_cols2
-struct matmat_thread_args { 
-    int tid;
-    double ** mat1; 
-    int num_cols1;
-    double ** mat2;
-    int num_cols2;
-    int partitionSize;
-    int partitionStart;
-    double ** res;
-};
-
-struct transpose_thread_args {
-    int tid;
-    double ** mat;
-    int num_cols;
-    int partitionSize;
-    int partitionStart;
-    double **res;
-};
-
-struct hadamard_thread_args {
-    int tid;
-    double ** mat1;
-    double ** mat2;
-    int num_cols;
-    int partitionSize;
-    int partitionStart;
-    double **res;
-};
-
 class MatrixCalculator {
     public:
-        MatrixCalculator(int numThreads);
+        MatrixCalculator();
 
         // actually useful functions
         void matrixTimesVector(double** mat, int num_rows, int num_cols, double** vec, int vec_size, double* result_vec);
@@ -59,42 +24,6 @@ class MatrixCalculator {
         void free_2D(double** arr);
 
         // random helper functions
-        
-
-        int numThreads;
-        pthread_t * threads;
-        
-    private:
-        static void* pMatTimesMat(void * thread_args) {
-            matmat_thread_args * args = (matmat_thread_args*) thread_args;
-            for (int i = args->partitionStart; i < args->partitionStart + args->partitionSize; i++)  {
-                for (int j = 0; j < args->num_cols2; j++)  {
-                    double sum = 0;
-                    for (int k = 0; k < args->num_cols1; k++)  {
-                        sum += args->mat1[i][k] * args->mat2[k][j]; 
-                    }
-                    args->res[i][j] = sum;
-                }
-            }    
-        }
-
-        static void* pTransposeMat(void * thread_args) {
-            transpose_thread_args * args = (transpose_thread_args*) thread_args;
-            for (int i = args->partitionStart; i < args->partitionStart + args->partitionSize; i++) {
-                for (int j = 0; j < args->num_cols; j++) {
-                    args->res[j][i] = args->mat[i][j];
-                }
-            }
-        }  
-
-        static void* pHadamardProd(void * thread_args) {
-            hadamard_thread_args * args = (hadamard_thread_args*) thread_args;
-            for (int i = args->partitionStart; i < args->partitionStart + args->partitionSize; i++) {
-                for (int j = 0; j < args->num_cols; j++) {
-                    args->res[i][j] = args->mat1[i][j] * args->mat2[i][j];
-                }
-            }
-        }  
 };
 
 #endif
